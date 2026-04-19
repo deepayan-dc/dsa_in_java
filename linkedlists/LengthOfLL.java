598aa99fa572ff50bab5e429637c2f9cd7f7ed3d
Deepayan Chakravarti <deepayan.chakravarti@gmail.com>
2026-04-19 16:00:41 +0530

    Added files

---
+++ linkedlists/LengthOfLL.java
@@ -0,0 +1,22 @@
+package linkedlists;
+
+public class LengthOfLL {
+    public static int getLength(Node head) {
+        int length = 0;
+        Node temp = head;
+        while (temp != null) {
+            length++;
+            temp = temp.next;
+        }
+        return length;
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+        System.out.println(getLength(head));
+    }
+}
