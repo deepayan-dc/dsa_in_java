251a9be4e719cf5de627e2a1f20bcd6d96dc16bd
Deepayan Chakravarti <deepayan.chakravarti@gmail.com>
2026-04-21 21:50:31 +0530

    Added files

---
+++ linkedlists/InsertionDeletion.java
@@ -0,0 +1,80 @@
+package linkedlists;
+
+public class InsertionDeletion {
+    public static Node convert (int[] arr) {
+        Node head = new Node (arr[0]);
+        Node curr = head;
+        for (int i = 1; i < arr.length; i++) {
+            curr.next = new Node (arr[i]);
+            curr = curr.next;
+        }
+        return head;
+    }
+
+    public static void print(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+
+    public static Node deleteHead(Node head){
+        if (head == null) return head;
+        head = head.next;
+        return head;
+    }
+
+    public static Node deleteTail(Node head){
+        if (head == null || head.next == null) return null;
+        Node temp = head;
+        while (temp.next.next != null) {
+            temp = temp.next;
+        }
+        temp.next = null;
+        return head;
+    }
+
+    public static Node deleteKthElement(Node head, int k){
+        if (head == null) return head;
+        if (k == 1) return deleteHead(head);
+        int count = 0;
+        Node temp = head;
+        Node prev = null;
+        while (temp != null){
+            count++;
+            if (count == k) {
+                prev.next = prev.next.next;
+                temp.next = null;
+                break;
+            }
+            prev = temp;
+            temp = temp.next;
+        }
+        return head;
+    }
+
+    public static Node deleteElement(Node head, int element){
+        if (head == null) return head;
+        if (head.data == element) return deleteHead(head);
+        Node temp = head;
+        Node prev = null;
+        while (temp != null){
+            if (temp.data == element) {
+                prev.next = prev.next.next;
+                temp.next = null;
+                break;
+            }
+            prev = temp;
+            temp = temp.next;
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        Node head = convert(arr);
+        print(deleteElement(head, 6));
+    }
+
+}
