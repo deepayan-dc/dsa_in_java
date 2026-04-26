73b0839a502da1534cb6229ee3299e0e992bf86e
Deepayan Chakravarti <deepayan.chakravarti@gmail.com>
2026-04-26 15:58:19 +0530

    Added and updated files

--- linkedlists/ReverseDLinkedList.java
+++ linkedlists/ReverseDLinkedList.java
@@ -1,5 +1,24 @@
 package linkedlists;
 
 public class ReverseDLinkedList {
+    public static DNode reverse(DNode head) {
+        DNode dummy = null;
+        DNode curr = head;
+        while (curr != null) {
+            curr = head.next;
+            head.next= dummy;
+            head.prev = curr;
+            dummy = head;
+            head = curr;
+        }
+        return dummy;
+    }
 
+    public static void main(String[] args) {
+        DNode head = ArrayToDLinkedList.convert(new int[]{1, 2, 3, 4, 5});
+        DNode.printList(head);
+        System.out.println();
+        head = reverse(head);
+        DNode.printList(head);
+    }
 }
